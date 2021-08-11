33eea59c0e7fb8281cc7efba06a8477e0de53de4
caputdraconis050630 <caputdraconis@kakao.com>
2021-08-11 10:48:57 +0900

    Add PingPong Function

--- main.py
+++ main.py
@@ -9,10 +9,18 @@ def main():
 
     client = commands.Bot(command_prefix=prefix, intents=intents)
 
+    @client.command(name = 'ping')
+    async def _ping(ctx):
+        await ctx.send("pong")
+
+
+
     with open('token.txt', 'r') as f:
         token = f.read()
 
     client.run(token)
+
+
 
 
 if __name__ == '__main__':
